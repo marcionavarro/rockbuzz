48eeaabbe556189454a6a072a93c113b0017a8e3
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2021-03-18 00:08:04 -0300

    Upgrading To 5.8.0 From 5.7

--- public/js/app.js
+++ public/js/app.js
@@ -48981,8 +48981,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\xampp\htdocs\fullstack-test-laravel\rockbuzz\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\xampp\htdocs\fullstack-test-laravel\rockbuzz\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\laragon\www\Dev\rockbuzz\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\laragon\www\Dev\rockbuzz\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
